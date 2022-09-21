e7548fbb11225af969718d7de076de615ddaf259
Hoàng Minh <hm1061998@gmail.com>
2022-09-21 17:26:12 +0700

    minh

--- AppNavigation.js
+++ AppNavigation.js
@@ -1,16 +1,17 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import { View } from 'react-native';
+import { View, Linking } from 'react-native';
 import {
   createStackNavigator,
   TransitionPresets,
 } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
-import * as Linking from 'expo-linking';
+// import * as Linking from 'expo-linking';
 import { useSelector } from 'react-redux';
 import RootRoute from '~/config/MainNavigation';
 // import RootRoute from '~/config/DrawerNavigation';
 import AuthNavigation from '~/config/auth';
+import * as Notifications from 'expo-notifications';
 
 const rootStack = createStackNavigator();
 
@@ -26,7 +27,59 @@ const AppNavigation = ({ deepLink }) => {
 
   return (
     <View onLayout={onLayout} style={{ flex: 1 }}>
-      <NavigationContainer fallback={null}>
+      <NavigationContainer
+        fallback={null}
+        linking={{
+          config: {
+            // Configuration for linking
+          },
+          async getInitialURL() {
+            // First, you may want to do the default deep link handling
+            // Check if app was opened from a deep link
+            let url = await Linking.getInitialURL();
+
+            if (url != null) {
+              return url;
+            }
+
+            // Handle URL from expo push notifications
+            const response =
+              await Notifications.getLastNotificationResponseAsync();
+            url = response?.notification.request.content.data.url;
+
+            return url;
+          },
+          subscribe(listener) {
+            const onReceiveURL = ({ url }) => listener(url);
+
+            // Listen to incoming links from deep linking
+            const linksubscription = Linking.addEventListener(
+              'url',
+              onReceiveURL,
+            );
+
+            // Listen to expo push notifications
+            const subscription =
+              Notifications.addNotificationResponseReceivedListener(
+                response => {
+                  const url = response.notification.request.content.data.url;
+
+                  // Any custom logic to see whether the URL needs to be handled
+                  //...
+
+                  // Let React Navigation handle the URL
+                  listener(url);
+                },
+              );
+
+            return () => {
+              // Clean up the event listeners
+              // Linking.removeEventListener('url', onReceiveURL);
+              linksubscription.remove();
+              subscription.remove();
+            };
+          },
+        }}>
         <rootStack.Navigator
           screenOptions={{
             headerShown: false,
--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,22 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
-    plugins: ['react-native-reanimated/plugin', 'module:react-native-dotenv'],
+    plugins: [
+      'react-native-reanimated/plugin',
+      'module:react-native-dotenv',
+      [
+        'babel-plugin-root-import',
+        {
+          rootPathPrefix: '~', // Chúng ta cần cho babel biết "~"
+          rootPathSuffix: '.', // sẽ được convert thành "."
+        },
+      ],
+    ],
+    retainLines: true,
+    env: {
+      production: {
+        plugins: ['transform-remove-console'],
+      },
+    },
   };
 };
